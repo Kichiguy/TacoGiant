aeb5cf710e0c06ab0a428e24fae3534134c66c2d
spragglerock <sprasarah@gmail.com>
2017-04-22 16:08:49 -0500

    update standard label style

--- src/game/button.js
+++ src/game/button.js
@@ -2,7 +2,7 @@ var StandardLabelButton = function(x, y, label, callback, callbackContext, overF
   let key = game.add.sprite(x, y, 'standardButtonSprite')
   Phaser.Button.call(this, game, x, y, key, callback, callbackContext, overFrame, outFrame, downFrame, upFrame);    
   
-  this.style = {'font': '10px Arial', 'fill': 'black' };
+  this.style = { font: "72px Helvetica", fill: "#fff", align: "center" };
   this.anchor.setTo( 0.5, 0.5 );
   this.label = new Phaser.Text(game, 0, 0, label, this.style); 
   //puts the label in the center of the button
