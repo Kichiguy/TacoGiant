ca146ddb6fb83bccc602f1ac5d3a5ac36f4943e8
Ben Wootten <benmwootten@gmail.com>
2017-04-22 10:11:20 -0500

    Merge

--- src/game/main.js
+++ src/game/main.js
@@ -7,38 +7,38 @@
  */
 
 var state = {
-    init: function() {
-        // Delete this init block or replace with your own logic.
+  init: function() {
+    // Delete this init block or replace with your own logic.
 
-        // Create simple text display for current Phaser version
-        // var text = "Phaser Version "+Phaser.VERSION + " works!";
-        // var style = { font: "24px Arial", fill: "#fff", align: "center" };
-        // var t = game.add.text(this.world.centerX, this.world.centerY, text, style);
-        // t.anchor.setTo(0.5, 0.5);
+    // Create simple text display for current Phaser version
+    // var text = "Phaser Version "+Phaser.VERSION + " works!";
+    // var style = { font: "24px Arial", fill: "#fff", align: "center" };
+    // var t = game.add.text(this.world.centerX, this.world.centerY, text, style);
+    // t.anchor.setTo(0.5, 0.5);
 
-    },
-    preload: function() {
-        // State preload logic goes here
-        game.load.image('taco', 'assets/taco.png');
-    },
-    create: function(){
-      // State create logic goes here
-        var s = game.add.image(200, 50, 'taco');
-        
-        var title = "Taco Giant";
-        var style = { font: "72px Arial", fill: "#00F", align: "center" };
-        var t = game.add.text(this.world.centerX, this.world.centerY, title, style);
-        t.anchor.setTo(0.5, 0.3);
-    },
-    update: function() {
-        // State Update Logic goes here.
-    }
+  },
+  preload: function() {
+    // State preload logic goes here
+    game.load.image('taco', 'assets/taco.png');
+  },
+  create: function(){
+  // State create logic goes here
+    var s = game.add.image(200, 50, 'taco');
+    
+    var title = "Taco Giant";
+    var style = { font: "72px Arial", fill: "#00F", align: "center" };
+    var t = game.add.text(this.world.centerX, this.world.centerY, title, style);
+    t.anchor.setTo(0.5, 0.3);
+  },
+  update: function() {
+    // State Update Logic goes here.
+  }
 };
 
 var game = new Phaser.Game(
-    800,
-    480,
-    Phaser.AUTO,
-    'game',
-    state
+  800,
+  480,
+  Phaser.AUTO,
+  'game',
+  state
 );
