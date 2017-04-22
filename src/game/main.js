e9105898208c4d703ce17f6e3c94e514e7cbb2ed
naomijsprague <naomi.j.sprague@gmail.com>
2017-04-22 11:45:45 -0500

    fix2

--- src/game/main.js
+++ src/game/main.js
@@ -16,6 +16,9 @@ var state = {
     // State preload logic goes here
     game.load.image('taco', 'assets/taco.png');
     game.load.image('giant', 'assets/sprites/PlaceholderGiant.png');
+    game.load.image('background', 'assets/sprites/PlaceholderBackground.png');
+    game.load.image('platform', 'assets/sprites/PlaceholderPlatform.png');
+    game.load.json('level:1', 'data/level01.json');
   },
   create: function(){
   // State create logic goes here
@@ -27,18 +30,24 @@ var state = {
     t.anchor.setTo(0.5, 0.3);
 
     player = new Player();
+
+    _loadLevel(this.game.cache.getJSON('level:1'));
   },
   update: function() {
     // State Update Logic goes here.
     player.update();
+  },
+  _loadLevel: function(data){
+    data.platforms.forEach(this._spawnPlatform, this);
+  },
+  _spawnPlatform: function(platform){
+    game.add.sprite(platform.x, platform.y, platform.image);
   }
 };
 
-var game = new Phaser.Game(
-  800,
-  480,
-  Phaser.AUTO,
-  'game',
-  state
-);
+window.onload = function () {
+    let game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
+    game.state.add('play', state);
+    game.state.start('play');
+};
 
