a10b04d76626dc1fa7db5e72be265379f17ecd33
spragglerock <sprasarah@gmail.com>
2017-04-23 15:43:35 -0500

    dont commit change to timer length

--- src/game/main.js
+++ src/game/main.js
@@ -77,7 +77,7 @@ var playState = {
 
     this._loadLevel(game.cache.getJSON('level:1'));
     score = new Score(0,0);
-    timer = new Timer(615,0,12);
+    timer = new Timer(615,0,120);
     menu = new PauseMenu(700, 50);
 
     //spawns the player
