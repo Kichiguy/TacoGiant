8277a21e627dbf621e81b39eb153a954e01628e9
spragglerock <sprasarah@gmail.com>
2017-04-23 19:34:25 -0500

    RELOAD TACOMETER

--- src/game/tacometer.js
+++ src/game/tacometer.js
@@ -2,17 +2,24 @@ function Tacometer(x,y,spriteTag){
   this.emitter = game.add.emitter(0, 0, 1000);
   this.emitter.fixedToCamera = true;
   this.emitter.makeParticles('tinierTaco');
-  
+  this.xCood = x;
+  this.yCood = y;
   //generates a vertical tacometer along the top edge
   Phaser.Group.call(this, game);
   //start with 10 tacos. 0 THROUGH 9
   this.tacoCount = 10;
-  this.initializeTacometer(x,y,spriteTag);
+  this.loadTacometer(x,y,spriteTag);
 
 }
 Tacometer.prototype = Object.create(Phaser.Group.prototype);
-Tacometer.prototype.initializeTacometer = function(x,y,spriteTag){
-  for(var i = this.tacoCount; i > 0; i -= 1){
+Tacometer.prototype.loadTacometer = function(x,y,spriteTag, toAdd = null){
+  if(toAdd){
+    var moreTacos = toAdd;
+  }else{
+    var moreTacos = this.tacoCount;
+  }
+  console.log("Loading Tacometer...." + moreTacos.toString() + " more tacos!")
+  for(var i = moreTacos; i > 0; i -= 1){
     var a = this.create(x,y,spriteTag);
     a.anchor.setTo(0.5, 0.5);
     a.fixedToCamera = true;
@@ -29,3 +36,19 @@ Tacometer.prototype.loseATaco = function(){
     this.emitter.start(true, 2000, null, 10);
   }
 };
+
+Tacometer.prototype.reloadTacometer = function(moreTacos){
+  var newTacos = this.tacoCount + moreTacos;
+  var oldCount = this.tacoCount;
+  this.tacoCount = Math.min(10, newTacos);
+  var moreTacos = this.tacoCount - oldCount;
+  var lastTaco = this.getTop();
+  if(lastTaco){
+    newTacoX = lastTaco.cameraOffset.x + 30;
+    newTacoY = lastTaco.cameraOffset.y;
+  }else{
+    newTacoX = this.xCood;
+    newTacoY = this.yCood;
+  }
+  this.loadTacometer(newTacoX, newTacoY, 'tinyTaco', moreTacos);
+}
